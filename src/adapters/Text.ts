bf6569ec318855e06eb0a912fc2e0d6c6672ba80
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-07 17:15:07 +0530

    Create Text Adapter

---
+++ src/adapters/Text.ts
@@ -0,0 +1,28 @@
+//  Library
+import * as fs from 'node:fs'
+
+//  Type Definitions
+import { Adapter } from '../types'
+
+//  ------------
+//  Text Adapter
+//  ------------
+
+export class TextAdapter<T> implements Adapter<string | Buffer> {
+
+    constructor(private fileName: string, private encoding?: BufferEncoding) {
+    }
+
+    async read(): Promise<string | Buffer> {
+        try {
+            return fs.promises.readFile(this.fileName, { encoding: this.encoding })
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async write(data: string | Buffer): Promise<void> {
+        return fs.promises.writeFile(this.fileName, data, { encoding: this.encoding })
+    }
+
+}
---
+++ src/adapters/index.ts
@@ -0,0 +1,5 @@
+//  --------
+//  ADAPTERS
+//  --------
+
+export * from './Text'
